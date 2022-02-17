a691556b4bf0e0e236096c85730426f0eb8a230b
Suphachai Phetthamrong <suphachaiphetthamrong@gmail.com>
2022-02-18 00:51:41 +0700

    Edit

--- Contracts/Cerisvice.sol
+++ Contracts/Cerisvice.sol
@@ -9,14 +9,12 @@ contract Certsvice is Owner {
         address issuer;
         string hash;
     }
-    int private certificateId = 0;
     mapping(address => string) private universitys;
-    mapping(int => student) private students;
+    mapping(string => student) private students;
 
     //reUseFunction
     function checkInuse(address _university) private view returns (bool) {
-        string memory uname = universitys[_university];
-        bool check = keccak256(abi.encodePacked(uname)) == keccak256(abi.encodePacked(""));
+        bool check = keccak256(abi.encodePacked(universitys[_university])) == keccak256(abi.encodePacked(""));
         //if check is false This address is already in use
         return check;
     }
@@ -27,7 +25,7 @@ contract Certsvice is Owner {
     }
     
     //myFunction for university
-    function addUniversity(address _university, string memory _universityName) public isOwner {
+    function addUniversity(address _university, string memory _universityName) public isOwner{
         require(checkInuse(_university), "This address is already in use");
         universitys[_university] = _universityName;
     }
@@ -36,21 +34,21 @@ contract Certsvice is Owner {
         delete universitys[_university];
     }
 
-    function getUniversity(address _university) public isOwner view returns (string memory) {
+    function getUniversity(address _university) public view returns (string memory) {
         return universitys[_university];
     }
     
     
     //myFunction for add student
-    function addStudent(string memory _hash) public isUnivesity returns(int){
+    function addStudent(string memory _hash,string memory _id) public isUnivesity {
         student memory newStudent = student(msg.sender,_hash);
-        students[certificateId++] = newStudent;
-        return certificateId;
+        students[_id] = newStudent;
+
     }
-    function deleteStudent(int _id) public isUnivesity{
+    function deleteStudent(string memory _id) public isUnivesity{
         delete students[_id];
     }
-    function getStudent(int _id) public view returns (student memory){
+    function getStudent(string memory _id) public view returns (student memory){
         return  students[_id];
     }
     
